61c43c9ab4492a9f016877cd08736c52a64dce78
Paco Guzmán <f.guzman@stuart.com>
2021-10-13 09:44:06 +0200

    Improve Istio dashboards and kube-prometheus integration

--- services/infrastructure/kube-prometheus/base/istio/get_dashboards.sh
+++ services/infrastructure/kube-prometheus/base/istio/get_dashboards.sh
@@ -14,5 +14,7 @@ for DASHBOARD in 7639 11829 7636 7630 7645; do
     REVISION="$(curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions -s | jq ".items[] | select(.description | contains(\"${VERSION}\")) | .revision")"
     curl -s https://grafana.com/api/dashboards/${DASHBOARD}/revisions/${REVISION}/download > "${SCRIPT_DIR}/${DASHBOARD}.json"
     echo "Storing $(jq -r '.title' ${SCRIPT_DIR}/${DASHBOARD}.json) (revision ${REVISION}, id ${DASHBOARD})..."
+    cat <<< $(jq '.__inputs[0].value = "prometheus"' "${SCRIPT_DIR}/${DASHBOARD}.json") > "${SCRIPT_DIR}/${DASHBOARD}.json"
+    sed -i '' 's/${DS_PROMETHEUS}/$datasource/g' "${SCRIPT_DIR}/${DASHBOARD}.json"
     echo -e "\nDone\n"
 done
